e4bf9cc6840f7eee69075fdaf17f0024a39f30d7
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2025-12-05 17:35:02 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,22 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Dev server options - allow specific hosts (useful when exposing dev server)
-  server: {
-    // allow network access to the dev server
-    host: true,
-    port: 5173,
-    // allowedHosts can include hostnames or patterns. Add your production preview domain
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      '0.0.0.0',
-      'https://imagify-backend-oe0c.onrender.com/',
-      '.onrender.com'
-    ],
-    hmr: {
-      // adjust HMR host if needed when accessing from other machines
-      host: 'localhost'
-    }
-  }
 })
